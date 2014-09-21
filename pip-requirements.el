8bc494afa0d4a7590252a1e8b8a5762958644a80
Wilfred Hughes <me@wilfred.me.uk>
2014-09-21 11:22:29 +0100

    Minor readme tweak.

--- pip-requirements.el
+++ pip-requirements.el
@@ -28,7 +28,10 @@
 
 ;;; Commentary:
 
-;; This mode adds basic highlighting for pip requirements files to Emacs.
+;; This is a major mode for editing pip requirements files, with the following features:
+
+;; * Syntax highlighting
+;; * Togglable comments
 
 ;; TODO: Steal shamelessly all the fantasic ideas in
 ;; https://github.com/wuub/requirementstxt
